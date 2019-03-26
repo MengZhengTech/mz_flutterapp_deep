daea7d7968aa4659749dabd1ec675f6f757f9c94
codehuicn <596947466@qq.com>
2019-03-26 13:54:42 +0800

    活动页的头部

---
+++ lib/views/activity/header_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// 地址、活动、搜索活动
+class HeaderBar extends StatefulWidget {
+  HeaderBar({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HeaderBarState createState() => new _HeaderBarState();
+}
+
+class _HeaderBarState extends State<HeaderBar> {
+  String _place = '厦门';
+
+  void _setPlace(String name) {
+    setState(() {
+      _place = name;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    return new ConstrainedBox(
+      constraints:
+        new BoxConstraints(minWidth: double.infinity, maxHeight: ScreenUtil().setWidth(90)),
+      child: Stack(
+        alignment: Alignment.center, //指定未定位或部分定位widget的对齐方式
+        children: <Widget>[
+          Container(
+            child: Text(widget.title,
+              style: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+                fontSize: ScreenUtil().setWidth(32)
+              )
+            ),
+          ),
+          Positioned(
+            left: ScreenUtil().setWidth(32),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(_place),
+                new Icon(Icons.keyboard_arrow_down),
+              ],
+            ),
+          ),
+          Positioned(
+            right: ScreenUtil().setWidth(32),
+            child: new Icon(Icons.search),
+          )
+        ],
+      ),
+    );
+  }
+}
